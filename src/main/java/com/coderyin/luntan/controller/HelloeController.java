7942bc086a13eaec5831609dc39f72c3df92845d
yinkailai <yinkailai@qq.com>
2020-03-08 11:13:44 +0800

    start

---
+++ src/main/java/com/coderyin/luntan/controller/HelloeController.java
@@ -0,0 +1,15 @@
+package com.coderyin.luntan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloeController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
